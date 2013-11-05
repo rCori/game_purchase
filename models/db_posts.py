8235b29f1537ee6ea6686797814533b1e71192b6
Ryan Cori <rcori@ucsc.edu>
2013-11-04 22:47:49 -0800

    Initial commit

---
+++ models/db_posts.py
@@ -0,0 +1,15 @@
+# coding: utf8
+
+#The table of posts of user's purchases submissions
+db.define_table('purchase',
+                Field('user_ref','reference auth_user'),
+                Field('title','string'),
+                Field('user_image','upload',),
+                Field('description','string'))
+
+
+#Comments that people can leave on a post
+db.define_table('rating',
+                Field('user_ref','reference auth_user'),
+                Field('purchase_ref','reference purchase'),
+                Field('rating','integer',requires=IS_INT_IN_RANGE(1,10)))
