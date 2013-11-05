e6c8c1a062e3730decb840d84fc5a205db86d9cf
Ryan Cori <rcori@ucsc.edu>
2013-11-05 00:47:43 -0800

    Took out some default web2py layout branding, headers and footers. If those were desired they can be put back by replacing the layout.html I edited with the default one. I left the admin controls in there for sake of development.

--- models/db_posts.py
+++ models/db_posts.py
@@ -4,7 +4,7 @@
 db.define_table('purchase',
                 Field('user_ref','reference auth_user'),
                 Field('title','string'),
-                Field('user_image','upload',),
+                Field('user_image','upload'),
                 Field('description','string'))
 
 
